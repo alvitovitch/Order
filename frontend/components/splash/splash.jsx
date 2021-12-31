85cfb84902e7003ae7f4be4e95f555f2435567ff
Andrew Vitovitch <alvitovitch@gmail.com>
2021-12-31 18:24:15 -0500

    have the rough text and buttons for the splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -9,7 +9,34 @@ class Splash extends React.Component {
     render() {
         return (
             <div>
-                This is the splash
+                <div>
+                    <ul>
+                        <div>
+                            Order
+                        </div>
+                        <div>
+                            <ul>
+                                <li>Download</li>
+                                <li>Nitro</li>
+                                <li>Safety</li>
+                                <li>Support</li>
+                                <li>Blog</li>
+                                <li>Careers</li>
+                            </ul>
+                        </div>
+                        <div>
+                            Login
+                        </div>
+                    </ul>
+                </div>
+                <div>
+                    <div>
+                        <h1>IMAGINE A PLACE...</h1>
+                        <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
+                    </div>
+                    <button>Download for Windows</button>
+                    <button>Open Order in your browser</button>
+                </div>
             </div>
         )
     }
