4431f46a1327a3ecb93cf8f97dc5d4fe498bf3be
Andrew Vitovitch <alvitovitch@gmail.com>
2022-03-10 13:43:09 -0500

    got rid of some blank space

--- frontend/components/home/homeContainer.js
+++ frontend/components/home/homeContainer.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { connect } from "react-redux";
 import { logoutSessionUser } from "../../actions/session_actions";
 import { fetchServers } from "../../actions/server_actions";
